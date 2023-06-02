3aad073ebefa823f655b91b00a9194eefa0d9665
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2023-06-02 15:54:32 +0200

    add intel-pyalf-req

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 images=(
-jupyter-pyalf-req
-jupyter-pyalf-full
-jupyter-pyalf-doc
-buster-pyalf-req
-bullseye-pyalf-req
-bookworm-pyalf-req
-bullseye-intel-pyalf-req
-bullseye-pgi-21-03-pyalf-req
+#jupyter-pyalf-req
+#jupyter-pyalf-full
+#jupyter-pyalf-doc
+#buster-pyalf-req
+#bullseye-pyalf-req
+#bookworm-pyalf-req
+#bullseye-intel-pyalf-req
+#bullseye-pgi-21-03-pyalf-req
+intel-pyalf-req
 )
 registry="git.physik.uni-wuerzburg.de:25812/alf/alf_docker"
 
