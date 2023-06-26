f8a39a26916e586135c2c080e5568df4d20a9838
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2023-06-26 13:20:40 +0200

    Also have a latest tag

--- build.sh
+++ build.sh
@@ -14,9 +14,11 @@ for name in "${names[@]}"; do
         if [[ -d $directory ]]; then
             fullname="${directory}:$(date --iso-8601)"
             echo "====== building $fullname ======"
-            docker build -t "$fullname" "$directory" || exit 1
-            docker tag "$fullname" "$registry/$fullname" || exit 1
-            docker push "$registry/$fullname" || exit 1
+            docker build -t "${directory}:$(date --iso-8601)" "$directory" || exit 1
+            docker tag "${directory}:$(date --iso-8601)" "$registry/${directory}:$(date --iso-8601)" || exit 1
+            docker tag "${directory}:$(date --iso-8601)" "$registry/${directory}:latest" || exit 1
+            docker push "$registry/${directory}:$(date --iso-8601)" || exit 1
+            docker push "$registry/${directory}:latest" || exit 1
         fi
     done
 done
