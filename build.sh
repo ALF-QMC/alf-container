6b989da517342c34be44b7650c8be5bdc438f5ac
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2023-06-26 11:18:27 +0200

    Express different distributions through repositories instead of tags

--- build.sh
+++ build.sh
@@ -10,11 +10,13 @@ pyalf-doc
 registry="git.physik.uni-wuerzburg.de:25812/alf/alf_docker"
 
 for name in "${names[@]}"; do
-    for directory in $name/*; do
-        fullname="$(echo $directory | sed s./.:.)"
-        echo "====== building $fullname ======"
-        docker build -t "$fullname" "$directory" || exit 1
-        docker tag "$fullname" "$registry/$fullname" || exit 1
-        docker push "$registry/$fullname" || exit 1
+    for directory in "$name"/*; do
+        if [[ -d $directory ]]; then
+            fullname="${directory}:$(date --iso-8601)"
+            echo "====== building $fullname ======"
+            docker build -t "$fullname" "$directory" || exit 1
+            docker tag "$fullname" "$registry/$fullname" || exit 1
+            docker push "$registry/$fullname" || exit 1
+        fi
     done
 done
