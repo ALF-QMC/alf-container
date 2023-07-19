d7997c22cf76089d36e1aa4a7e5c7e5fbb04717d
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2023-07-19 14:15:13 +0200

    update

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ registry="git.physik.uni-wuerzburg.de:25812/alf/alf_docker"
 for name in "${names[@]}"; do
     for directory in "$name"/*; do
         if [[ -d $directory ]]; then
-            fullname="${directory}:$(date --iso-8601)"
-            echo "====== building $fullname ======"
-            docker build -t "${directory}:$(date --iso-8601)" "$directory" || exit 1
-            docker tag "${directory}:$(date --iso-8601)" "$registry/${directory}:$(date --iso-8601)" || exit 1
-            docker tag "${directory}:$(date --iso-8601)" "$registry/${directory}:latest" || exit 1
+            echo "====== building ${directory} ======"
+            docker build -t "${directory}:latest" "$directory" || exit 1
+            docker tag "${directory}:latest" "${directory}:$(date --iso-8601)" || exit 1
+            docker tag "${directory}:latest" "$registry/${directory}:$(date --iso-8601)" || exit 1
+            docker tag "${directory}:latest" "$registry/${directory}:latest" || exit 1
             docker push "$registry/${directory}:$(date --iso-8601)" || exit 1
             docker push "$registry/${directory}:latest" || exit 1
         fi
